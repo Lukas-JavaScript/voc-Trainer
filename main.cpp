950db57e04273760ba717576ca6a1bb1e7472990
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-14 16:15:53 +0100

    add choice

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 int main() {
 	cout << "Welcome to the vocabulary learner!\n";
-	cout << "This program helps you learn new words.";
+	cout << "This program helps you learn new words.\n";
+	cout << "Let's get started!\n";
+	cout << "Please enter 1 or 2\n";
+	cout << "1. Add a new word\n";
+	cout << "2. Start test\n";
+	cout << "Your choice: ";
+	string choice;
+	cin >> choice;
 	return 0;
 }
