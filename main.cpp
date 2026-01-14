3dc11fc596b4e7a733e6d2c430b451c51f6afdad
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-14 16:28:38 +0100

    add more logic

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+	string choice;
 	cout << "Welcome to the vocabulary learner!\n";
 	cout << "This program helps you learn new words.\n";
 	cout << "Let's get started!\n";
@@ -11,10 +12,17 @@ int main() {
 	cout << "1. Add a new word\n";
 	cout << "2. Start test\n";
 	cout << "Your choice: ";
-	string choice;
 	cin >> choice;
+	cout << "You entered: " + choice + "\n";
 	if (choice == "1") {
 		cout << "You chose to add a new word.\n";
+		cout << "Which word would you like to add? (german): ";
+		string german;
+		string english;
+		cin >> german;
+		cout << "Enter the English translation: ";
+		cin >> english;
+		cout << "You added the word: " + german + " - " + english + "\n";
 		// Code to add a new word would go here
 	} else if (choice == "2") {
 		cout << "You chose to start the test.\n";
