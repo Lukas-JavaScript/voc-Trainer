9c5e35455a0ea250596cc5101277965d351b78fd
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-15 11:32:08 -0500

    add void start() {

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 #include <iterator>  
 
 using namespace std;
+void start() {
+	cout << "Welcome to the vocabulary learner!\n"; 
+	cout << "This program helps you learn new words.\n";
+	cout << "Let's get started!\n";
+	cout << "Please enter 1 or 2\n";
+	cout << "1. Add a new word\n";
+	cout << "2. Start test\n";
+	cout << "Your choice: ";
+}
 
 int main() {
 	bool exit = false;
@@ -20,13 +29,7 @@ int main() {
 		while (getline(english_file, line)) {
 			voc_english.push_back(line);
 		}
-	cout << "Welcome to the vocabulary learner!\n"; 
-	cout << "This program helps you learn new words.\n";
-	cout << "Let's get started!\n";
-	cout << "Please enter 1 or 2\n";
-	cout << "1. Add a new word\n";
-	cout << "2. Start test\n";
-	cout << "Your choice: ";
+	start();
 	cin >> choice;
 	cout << "You entered: " + choice + "\n";
 	if (choice == "1") {
