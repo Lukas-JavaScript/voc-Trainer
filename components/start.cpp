4b4e4228199ed9a4e0e1b3807830f5bdc0a1e9d7
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-17 02:08:50 -0500

    add component start

---
+++ components/start.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void start() {
+	cout << "Welcome to the vocabulary learner!\n"; 
+	cout << "This program helps you learn new words.\n";
+	cout << "Let's get started!\n";
+	cout << "Please enter 1 or 2\n";
+	cout << "1. Add a new word\n";
+	cout << "2. Start test\n";
+	cout << "Your choice: ";
+}
--- main.cpp
+++ main.cpp
@@ -3,18 +3,9 @@
 #include <vector>
 #include <fstream>
 #include <iterator>  
+#include "components/start.hpp"
 
 using namespace std;    
-
-void start() {
-	cout << "Welcome to the vocabulary learner!\n"; 
-	cout << "This program helps you learn new words.\n";
-	cout << "Let's get started!\n";
-	cout << "Please enter 1 or 2\n";
-	cout << "1. Add a new word\n";
-	cout << "2. Start test\n";
-	cout << "Your choice: ";
-}
 
 string used_choice() {
 	string choice;
