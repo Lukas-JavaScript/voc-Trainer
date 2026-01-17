ffc567109f7eb7e8dfe7a5ef3342c59957c56810
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-17 01:50:52 -0500

    fixed

--- main.cpp
+++ main.cpp
@@ -47,9 +47,9 @@ bool exit_program(bool exit) {
 }
 
 void add(vector<string>& voc_german, vector<string>& voc_english, bool& exit) {
+	cout << "You chose to add a new word.\n";
+	cout << "Which word would you like to add? (german): ";
 	while(!exit) {
-		cout << "You chose to add a new word.\n";
-		cout << "Which word would you like to add? (german): ";
 		string german_word;
 		string english_word;
 		cin >> german_word;
@@ -70,22 +70,22 @@ void add(vector<string>& voc_german, vector<string>& voc_english, bool& exit) {
 }
 
 void start_test(const vector<string>& voc_german, const vector<string>& voc_english, bool& exit) {
-		while (!exit) {
-			cout << "You chose to start the test.\n";
-			cout << "Test is starting...\n";
-			int random = rand() % voc_german.size();
-			string selected_element = voc_german[random];
-			cout << "What is the English translation of the German word: " + selected_element + "?\n";
-			cout << "Your answer: ";
-			string user_answer;
-			cin >> user_answer;
-			if (user_answer == voc_english[random]) {
-				cout << "Correct! Well done. All right!\n";
-			} else {
-				cout << "Incorrect. The correct translation is: " + voc_english[random] + "\n";
-			}
-			exit = exit_program(exit);
+	cout << "You chose to start the test.\n";
+	cout << "Test is starting...\n";
+	while (!exit) {
+		int random = rand() % voc_german.size();
+		string selected_element = voc_german[random];
+		cout << "What is the English translation of the German word: " + selected_element + "?\n";
+		cout << "Your answer: ";
+		string user_answer;
+		cin >> user_answer;
+		if (user_answer == voc_english[random]) {
+			cout << "Correct! Well done. All right!\n";
+		} else {
+			cout << "Incorrect. The correct translation is: " + voc_english[random] + "\n";
 		}
+		exit = exit_program(exit);
+	}
 }
 int main() {
 	bool exit = false;
