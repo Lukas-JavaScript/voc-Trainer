6cba0f93e1fd1823adeecff924e081c5e70fa466
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-01-19 11:43:08 -0500

    fixed

--- components/exit_program/exit_program.cpp
+++ components/exit_program/exit_program.cpp
@@ -5,23 +5,21 @@
 using namespace std;
 
 bool exit_program(bool exit) {
-	cout << "Do you want to continue? (y/n)(default:y): ";
-	string continue_choice;
-	cin.ignore();
-	getline(cin, continue_choice);
-	if (continue_choice == "n") {
-		exit = true;
-		cout << "Exiting the program. Goodbye!\n";
-	} 
-	else if (continue_choice == "y") {
-		exit = false;
-	}
-	else if (continue_choice == "") {
-		exit = false;
-	} 
-	else {
-		cout << "Invalid choice. Exiting the test. Let's continue!\n";
-		exit = false;
-	}
-	return exit;
+    cout << "Do you want to continue? (y/n)(default:y): ";
+    string continue_choice;
+    cin.ignore();
+    getline(cin, continue_choice);
+    
+    if (continue_choice == "n" || continue_choice == "N") {
+        exit = true;
+        cout << "Exiting the program. Goodbye!\n";
+    } 
+    else if (continue_choice == "y" || continue_choice == "Y" || continue_choice == "") {
+        exit = false;
+    }
+    else {
+        cout << "Invalid choice. Continuing...\n";
+        exit = false;
+    }
+    return exit;
 }
--- components/start_test.cpp
+++ components/start_test.cpp
@@ -8,20 +8,22 @@
 using namespace std;
 
 void start_test(const vector<string>& voc_german, const vector<string>& voc_english, bool& exit) {
-	cout << "You chose to start the test.\n";
-	cout << "Test is starting...\n";
-	while (!exit) {
-		int random = rand() % voc_german.size();
-		string selected_element = voc_german[random];
-		cout << "What is the English translation of the German word: " + selected_element + "?\n";
-		cout << "Your answer: ";
-		string user_answer;
-		cin >> user_answer;
-		if (user_answer == voc_english[random]) {
-			cout << "Correct! Well done. All right!\n";
-		} else {
-			cout << "Incorrect. The correct translation is: " + voc_english[random] + "\n";
-		}
-		exit = exit_program(exit);
-	}
+    cout << "You chose to start the test.\n";
+    cout << "Test is starting...\n";
+    while (!exit) {
+        int random = rand() % voc_german.size();
+        string selected_element = voc_german[random];
+        cout << "What is the English translation of the German word: " << selected_element << "?\n";
+        cout << "Your answer: ";
+        string user_answer;
+        cin.ignore();
+        getline(cin, user_answer);
+        if (user_answer == voc_english[random]) {
+            cout << "Correct! Well done. All right!\n";
+        } else {
+            cout << "Incorrect. The correct translation is: " << voc_english[random] << "\n";
+        }
+        exit_program(exit);
+		cout << exit << "\n";
+    }
 }
